a3a02dc66038c70c5f6dfd066f33726db8750523
cookkkie <cookie.io@openmailbox.org>
2017-09-18 13:49:30 +0200

    Removed Backbone init from app start

--- lib/citadel/application.ex
+++ lib/citadel/application.ex
@@ -2,18 +2,14 @@ defmodule Citadel.Application do
   use Application
 
   alias Citadel.Utils.Partitioner
-  alias Citadel.{Registry, Groups, Backbone}
+  alias Citadel.{Registry, Groups}
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-
-    Backbone.init()
-
     children = [
       Partitioner.worker(Registry, Citadel.Registry.Partitioner),
       Partitioner.worker(Groups, Citadel.Groups.Partitioner)
     ]
-
     opts = [strategy: :one_for_one, name: Citadel.Supervisor]
     Supervisor.start_link(children, opts)
   end
